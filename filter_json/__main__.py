520d7c4f29fd3575798cbd77759fa2aa7f75477d
Rodrigo Perez <rodri.perez.1998@gmail.com>
2020-12-18 17:17:14 -0300

    Submited .csv excercise and previous json excercises

--- filter_json/__main__.py
+++ filter_json/__main__.py
@@ -1,10 +1,10 @@
 import json
 import timeit
 
-with open("output.json", "r") as f:
-    data = json.load(f)
 
 def inputs():
+    with open("output.json", "r") as f:
+        data = json.load(f)
 
     print("Escoja parámetro por el que desee filtrar.\n1- Nombre\n2- País")
 
@@ -12,9 +12,9 @@ def inputs():
 
     if choice == "1":
         name = input("Ingrese el nombre que desee filtrar: ")
-        if name.capitalize() in ["Fran","Harmy","Gun","Enzo","Rodri"]:
+        if name.capitalize() in ["Fran", "Harmy", "Gun", "Enzo", "Rodri"]:
             value = "Name"
-            return filter(name,value)
+            return filter(name, value, data)
         else:
             print("El nombre ingresado no se encuentra en la base de datos, vuelva a intentarlo.")
 
@@ -22,19 +22,20 @@ def inputs():
         name = input("Ingrese el país que desee filtrar: ")
         if name.capitalize() in get_countries(data):
             value = "Country"
-            return filter(name,value)
+            return filter(name, value, data)
         else:
             print("El país ingresado no se encuentra en la base de datos, vuelva a intentarlo.")
 
     else:
         print("Parámetro incorrecto, inténtelo de nuevo.")
 
-def filter(name, value):
 
+def filter(name, value, data):
     new_output_name = [x for x in data if x[value] == name.capitalize()]
 
     with open("new_output_%s.json" % name, "w") as file:
-        json.dump(new_output_name, file,indent=2)
+        json.dump(new_output_name, file, indent=2)
+
 
 def get_unique_items(iterable):
     seen = set()
@@ -45,21 +46,23 @@ def get_unique_items(iterable):
             result.append(item)
     return result
 
+
 def get_countries(data):
-    lista_paises=[]
+    lista_paises = []
 
     for x in data:
         lista_paises.append(x["Country"])
 
     return get_unique_items(lista_paises)
 
+
 def main():
     start = timeit.default_timer()
 
     inputs()
 
-    print("Al programa le llevo %s segundos terminar" %round(timeit.default_timer()-start,2))
+    print("Al programa le llevo %s segundos terminar" % round(timeit.default_timer() - start, 2))
+
+
 if __name__ == "__main__":
     main()
-
-
